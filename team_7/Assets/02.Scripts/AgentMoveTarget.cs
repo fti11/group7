9c8784028280a01778f9333b6f4cb51427514c15
shingugitvr000 <shingugitvr000@gmail.com>
2023-06-15 11:24:32 +0900

    NavMesh 적 캐릭터 이동 설정

---
+++ team_7/Assets/02.Scripts/AgentMoveTarget.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMoveTarget : MonoBehaviour
+{
+    public enum AGENTTYPE
+    {
+        ENEMY,
+        PLAYER,
+        PET
+    }
+
+
+    public Vector3 genPoint;
+
+    public float searchTimer = 0.0f;
+    public float searchLoop = 2.0f;
+
+    public NavMeshAgent agent;
+
+    public AGENTTYPE agenttype = AGENTTYPE.ENEMY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        genPoint = this.gameObject.transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = genPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoSearchLoop();
+       
+    }
+
+    void DoSearchLoop()
+    {
+        searchTimer += 1;
+        if (searchLoop <= searchTimer)
+        {
+            searchTimer = 0;
+
+            if(agenttype == AGENTTYPE.ENEMY)
+            {
+                GameObject temp = GameObject.FindGameObjectWithTag("Player");
+
+                if (temp != null)
+                {
+                    if(Vector3.Distance(temp.transform.position , this.gameObject.transform.position) < 10.0f)
+                    {
+                        agent.destination = temp.transform.position;
+                    }
+                    else
+                    {
+                        agent.destination = genPoint;
+                    }
+                  
+                }
+            }
+          
+        }
+    }
+}
